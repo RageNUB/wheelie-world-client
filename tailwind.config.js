e86c6ef7ecf83aa7d13ee41ee12d684b74ddac89
RageNUB <jirimon39@gmail.com>
2023-05-18 15:53:10 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#2cf79f",
+
+          "secondary": "#83fce6",
+
+          "accent": "#c43663",
+
+          "neutral": "#1c1917",
+
+          "base-100": "#f3f4f6",
+
+          "info": "#c084fc",
+
+          "success": "#33E18A",
+
+          "warning": "#fbbf24",
+
+          "error": "#f43f5e",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
