bae21c8fdc8905b8779257163435b3767bd59fab
RageNUB <jirimon39@gmail.com>
2023-05-19 01:01:11 +0600

    firebase auth initialize

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQToY1z7uo-jdYsOLyWtUKtfqJLaInRaU",
+  authDomain: "wheelie-world-83291.firebaseapp.com",
+  projectId: "wheelie-world-83291",
+  storageBucket: "wheelie-world-83291.appspot.com",
+  messagingSenderId: "937107728962",
+  appId: "1:937107728962:web:fbb5dd88b011c7bfb3dbdb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,65 @@
+import { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const googleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    } 
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth);
+    }
+
+    const passwordReset = (email) =>{
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    useEffect( () => {
+       const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log("auth state change", currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        setUser,
+        createUser,
+        signIn,
+        logOut,
+        googleSignIn,
+        passwordReset
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
